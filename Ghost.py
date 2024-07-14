7dbed38badf9fd98db405d2f6a62d6405ff34ccd
michael-kabat <michaelckabat@gmail.com>
2024-07-13 20:19:30 -0700

    cleaning stuff up

--- Ghost.py
+++ Ghost.py
@@ -1,26 +1,26 @@
-from pygame import *
+import pygame
+from pygame import Vector2
 import random
 
 class Ghost:
 
-    def __init__(self, pygame, screen, radius, color, speed) -> None:
-        self.pygame = pygame
+    def __init__(self, screen, radius, color, speed) -> None:
+        
         self.screen = screen
         
         self.radius = radius
         self.color = color
-        self.speed = speed
         self.state = False
-        self.velocity = Vector2(random.randrange(-10, 10), random.randrange(-10, 10))
+        self.velocity = Vector2(random.randint(-speed, speed), random.randint(-speed, speed))
 
-        self.pos : Vector2 = Vector2(random.randint(0, self.screen.get_width() - self.radius * 2), 
-                                     random.randint(0, self.screen.get_height() - self.radius * 2))
+        self.pos : Vector2 = Vector2(random.randint(self.radius * 2, self.screen.get_width() - (self.radius * 2)), 
+                                     random.randint(self.radius * 2, self.screen.get_height() - (self.radius * 2)))
 
 
-    def chase(self, player_pos:Vector2, framerate):
-        dx = (self.pos.x - player_pos.x) / framerate
-        dy = (self.pos.y - player_pos.y) / framerate
-        self.pos -= self.pygame.Vector2(dx, dy).normalize() * self.speed
+    # def chase(self, player_pos:Vector2, framerate):
+    #     dx = (self.pos.x - player_pos.x) / framerate
+    #     dy = (self.pos.y - player_pos.y) / framerate
+    #     self.pos -= self.pygame.Vector2(dx, dy).normalize() * self.speed
 
     def ghost_boundries(self):
 
@@ -30,10 +30,11 @@ class Ghost:
             self.velocity.y *= -1
 
 
-    def move(self):
-        self.pos += self.velocity
+    def move(self, dt):
+        self.pos += self.velocity * dt
 
 
     def draw_ghost(self):
-        self.pygame.draw.circle(self.screen, self.color, self.pos, self.radius)
+        pygame.draw.circle(self.screen, self.color, self.pos, self.radius)
+        
 
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# Example file showing a circle moving on screen
 import pygame
 from Apple import Apple
 from Ghost import Ghost
 from Player import Player
 
 
+STATE = 'MENU'
+SCORE = 0
 
 class Game:
     
@@ -21,16 +22,15 @@ class Game:
     apple_list: list[Apple] = []
     ghost_list : list[Ghost] = []
 
-    SCORE = 0
+    apple_dict : dict[int, Apple] = dict()
     
-    radius = 40
+    radius = 35
 
     player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
     font = pygame.font.Font('freesansbold.ttf', 28)
 
     player = Player(screen, radius, 375)
-
-    STATE = 'RUNNING'
+    
 
     while running:
         # poll for events
@@ -41,7 +41,33 @@ class Game:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_down = True
+        
+        if STATE == "MENU":
+            pygame.mouse.set_visible(True)
+            mouse = pygame.mouse.get_pos()
+            color = "grey"
+            
+
+            if (mouse[0] > screen.get_width() / 2 - 70) and mouse[0] < screen.get_width() / 2 + 70:
+                if mouse[1] > screen.get_height() / 2 and mouse[1] < screen.get_height() / 2 + 40:
+                    color = "light grey"
                 
+                    if mouse_down:
+                        STATE = 'RUNNING'
+                        ghost_list = []
+                        apple_list = []
+                        SCORE = 0
+                        player.pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+                        mouse_down = False
+                        
+
+            retry_button = pygame.draw.rect(screen, color, [screen.get_width()/2 - 70,screen.get_height()/2,140,40])
+            text = font.render("Play", True, (0, 0, 0))
+            textRect = text.get_rect()
+            
+            textRect.center = (screen.get_width()/2,screen.get_height()/2 + 20)
+            screen.blit(text, textRect)
+
         if STATE == 'RUNNING':
         # fill the screen with a color to wipe away anything from last frame
             screen.fill("dark grey")
@@ -49,45 +75,58 @@ class Game:
             # hide the mouse
             pygame.mouse.set_visible(False)
             
-            
-            # player movement  
-            player.draw()
+            # player control 
+
             player.move(dt)
-            
 
             # spawning ghosts
-            
-            if len(ghost_list) <= SCORE / 5:
-                ghost_list.append(Ghost(pygame, screen, 20, "blue", 2))
-                
-
+                            
             for ghost in ghost_list:
-                ghost.draw_ghost()
-                # ghost.chase(player_pos, 60)
-                ghost.move()
                 ghost.state = player.check_collisions(ghost)
+                ghost.draw_ghost()
+                ghost.move(dt)
+
+                # player movement  
+                player.draw()
+
+                ghost.ghost_boundries()
+
                 if ghost.state:
                     STATE = 'GAME OVER'
             
+
+            
             # ghost wall boundries 
-            for ghost in ghost_list:
-                ghost.ghost_boundries()
             
             # spawning apples
+            
             if len(apple_list) < 5:
                 apple_list.append(Apple(pygame, screen, screen.get_width(), screen.get_height(), 15))
 
             for apple in apple_list:
                     apple.draw_apple()
                     apple.state = player.check_collisions(apple)
-                    
-        
-            for apple in apple_list:
-                if apple.state:
-                    apple_list.remove(apple)
-                    SCORE += 1
+                    if apple.state:
+                        apple_list.remove(apple)
+                        SCORE += 1
 
+            # if len(apple_dict) < 5:
+            #     i = 0
+            #     while len(apple_dict) < 5:
+            #         apple_dict[i] = Apple(pygame, screen, screen.get_width(), screen.get_height(), 15)
+            #         i += 1
+
+            # for key in apple_dict:
+            #         apple_dict[key].draw_apple()
+            #         if player.check_collisions(apple_dict[key]):
+            #             apple_dict.pop(key)
+            #             SCORE += 1
             
+        
+
+            # Spawning ghosts 
+            if len(ghost_list) <= SCORE / 5:
+                ghost_list.append(Ghost( screen, 20, "blue", 200))
 
             # display score 
 
@@ -104,10 +143,7 @@ class Game:
             textRect.center = (screen.get_width() - textRect.left - textRect.right / 2, 20)
             screen.blit(text, textRect)
 
-            # flip() the display to put your work on screen
-
             
-
             # limits FPS to 60
             # dt is delta time in seconds since last frame, used for framerate-
             # independent physics.
@@ -116,7 +152,7 @@ class Game:
         if STATE == "GAME OVER":
             pygame.mouse.set_visible(True)
             mouse = pygame.mouse.get_pos()
-            color = "black"
+            color = "grey"
             
 
             if (mouse[0] > screen.get_width() / 2 - 70) and mouse[0] < screen.get_width() / 2 + 70:
@@ -124,7 +160,6 @@ class Game:
                     color = "light grey"
                 
                     if mouse_down:
-                        print("uo?")
                         STATE = 'RUNNING'
                         ghost_list = []
                         apple_list = []
@@ -134,13 +169,14 @@ class Game:
                         
 
             retry_button = pygame.draw.rect(screen, color, [screen.get_width()/2 - 70,screen.get_height()/2,140,40])
-
-            text = font.render("Continue", True, (0, 0, 0))
+            text = font.render("Retry", True, (0, 0, 0))
             textRect = text.get_rect()
             
             textRect.center = (screen.get_width()/2,screen.get_height()/2 + 20)
             screen.blit(text, textRect)
-            
+
+        # flip() the display to put your work on screen  
+        #  
         pygame.display.flip()
 
     pygame.quit()
